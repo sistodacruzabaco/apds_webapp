c5529deb52520ce47bf0fde855c03a74fc972c58
sistodacruzabaco <sistodacruzabaco@gmail.com>
2024-04-06 19:34:08 +0200

    editing db

--- prisma/migrations/20240404223621_create_db_structure/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `gender` on table `employees` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `employees` MODIFY `gender` ENUM('MASCULINO', 'FEMININO', 'OUTRO') NOT NULL DEFAULT 'MASCULINO';
