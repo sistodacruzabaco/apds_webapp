54691e90c4cd89b48c9dbfeb489ee250f4e886cc
sistodacruzabaco <sistodacruzabaco@gmail.com>
2024-04-07 23:41:10 +0200

    restruturando a API

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require('express');
-// const { PrismaClient } = require('@prisma/client');
-// const { Prisma } = require('@prisma/client');
-// const prisma = new PrismaClient();
 const cors = require('cors');
 
-const usersRouter = require('./routes/users');
-const projectsRouter = require('./routes/projects');
-const EmployeesRouter = require('./routes/employees');
-
+const projectRouters = require('./routes/projectRoutes');
+const employeeRouters = require('./routes/employeeRoutes');
+const feedRouters = require('./routes/feedRoutes');
+const userRouters = require('./routes/userRoutes');
 
 const app = express();
-
+app.use(express.json()); // Parse JSON request bodies\
 app.use(cors());
-app.use(express.json()); // Parse JSON request bodies
-app.use('/api', usersRouter); // Use the /api prefix for your routes
-app.use('/api', projectsRouter); 
-app.use('/api', EmployeesRouter); 
+
+app.use('/api', userRouters); // Use the /api prefix for your routes
+app.use('/api', projectRouters); 
+app.use('/api', employeeRouters); 
+app.use('/api', feedRouters);  
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
