2a8fea917b08e2a427629bd9c0cf1ea87334fe78
sistodacruzabaco <sistodacruzabaco@gmail.com>
2024-04-07 12:22:40 +0200

    adding cors

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 // const { PrismaClient } = require('@prisma/client');
 // const { Prisma } = require('@prisma/client');
 // const prisma = new PrismaClient();
+const cors = require('cors');
 
 const usersRouter = require('./routes/users');
 const projectsRouter = require('./routes/projects');
 const EmployeesRouter = require('./routes/employees');
 
+
 const app = express();
+
+app.use(cors());
 app.use(express.json()); // Parse JSON request bodies
 app.use('/api', usersRouter); // Use the /api prefix for your routes
 app.use('/api', projectsRouter); 
